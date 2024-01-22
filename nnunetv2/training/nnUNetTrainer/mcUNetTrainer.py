b1d8db250b178d279879087ccdad9edf72c9b65b
Xinzi He <51917510+Novestars@users.noreply.github.com>
2024-01-21 21:48:39 -0500

    Update mcUNetTrainer.py

--- nnunetv2/training/nnUNetTrainer/mcUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/mcUNetTrainer.py
@@ -59,7 +59,7 @@ class MultiClassDeepSupervisionWrapper(nn.Module):
 
         labels_per_image_list = [target[0][i:i+1].unique() for i in range(len(target[0].shape[0]))]
         for i in range(len(labels_per_image_list)):
-            if (labels_per_image_list[i] >= class_ind_start_end[0][1]).any():
+            if (labels_per_image_list[i] >= self.dataset_labels_description[0][1]).any():
                 labels_per_image_list[i] = labels_per_image_list[i][labels_per_image_list[i] != 0]
 
         dataset_ind_list = []
