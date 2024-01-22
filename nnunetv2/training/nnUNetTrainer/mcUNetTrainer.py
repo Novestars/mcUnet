40217a2c938f4dd0ba608e3e6cbd752af4865026
Novestars <841023629@qq.com>
2024-01-21 20:46:35 -0500

    Second commit

--- nnunetv2/training/nnUNetTrainer/mcUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/mcUNetTrainer.py
@@ -1,6 +1,8 @@
 import torch
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 from nnunetv2.training.loss.robust_ce_loss import RobustCrossEntropyLoss
+from nnunetv2.training.loss.deep_supervision import DeepSupervisionWrapper
+from nnunetv2.training.loss.compound_losses import DC_and_CE_loss, DC_and_BCE_loss
 import numpy as np
 import torch.nn as nn
 
